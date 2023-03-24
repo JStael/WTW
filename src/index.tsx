e9395ceb28e4f3b9b68b2f2dd45f87044f75a6d6
Joao Stael <joaostael@gmail.com>
2023-03-24 10:33:14 -0300

    added new lib react-native-vector-icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
 
 function App(): JSX.Element {
-  return <Text>Olá Mundo</Text>;
+  return <View></View>;
 }
 
 export default App;
