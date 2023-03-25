9b54dd887597747af782547b802a530fdc0935f3
Joao Stael <joaostael@gmail.com>
2023-03-24 23:22:52 -0300

    fixed statusbar style

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { AppStack } from './AppStack';
 import { OnboardingStack } from './OnboardingStack';
+import { StatusBar } from 'react-native';
+import { getColor } from '../utils/helpers/getTheme';
 
 export const AppRoutes = () => {
-  const onFirst = false;
+  const onFirst = true;
+  const backgroundColor = getColor('background');
   return (
     <NavigationContainer>
+      <StatusBar barStyle={'light-content'} backgroundColor={backgroundColor} />
       {onFirst ? <OnboardingStack /> : <AppStack />}
     </NavigationContainer>
   );
