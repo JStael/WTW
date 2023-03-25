6b81bc0e448651ba0573946e556eb673dcd52e5f
Joao Stael <joaostael@gmail.com>
2023-03-24 23:19:25 -0300

    added onboarding images

---
+++ src/assets/Onboarding1.tsx
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import Svg, { G, Defs, Pattern, Path, Text } from 'react-native-svg';
+/* SVGR has dropped some elements not supported by react-native-svg: style */
+
+export const Onboarding1 = () => {
+  return (
+    <Svg
+      width={295}
+      height={245}
+      id="screenshot-764161a2-819e-8085-8002-40e24bcfde3d"
+      viewBox="0 0 295 245"
+      fill="none">
+      <G id="shape-764161a2-819e-8085-8002-40e24bcfde3d">
+        <G id="shape-b37a7557-9722-8090-8002-3f00c5b74d57">
+          <Defs>
+            <Pattern
+              patternUnits="userSpaceOnUse"
+              x={0}
+              y={172}
+              height={73}
+              width={295}
+              data-loading="false"
+              id="fill-0-rumext-id-2">
+              <Path d="M0 0H295V73H0z" fill="#fff" fillOpacity={1} />
+            </Pattern>
+            <Pattern
+              patternUnits="userSpaceOnUse"
+              x={0}
+              y={172}
+              height={73}
+              width={295}
+              data-loading="false"
+              id="fill-1-rumext-id-2">
+              <Path d="M0 0H295V73H0z" fill="#fff" fillOpacity={1} />
+            </Pattern>
+          </Defs>
+          <G className="text-container">
+            <Text
+              x={74}
+              y={174}
+              textLength={142.23333740234375}
+              lengthAdjust="spacingAndGlyphs"
+              id="fills-b37a7557-9722-8090-8002-3f00c5b74d57"
+              letterSpacing="normal"
+              fontStyle="normal"
+              fontWeight={400}
+              fontSize="24px"
+              fill="#fff"
+              fillOpacity={1}>
+              {'Bem vindo ao'}
+            </Text>
+            <Text
+              x={66}
+              y={210}
+              textLength={152.39999389648438}
+              lengthAdjust="spacingAndGlyphs"
+              id="fills-b37a7557-9722-8090-8002-3f00c5b74d57"
+              letterSpacing="normal"
+              fontStyle="normal"
+              fontWeight={400}
+              fontSize="24px"
+              fill="#fff"
+              fillOpacity={1}>
+              {'What to Watch!'}
+            </Text>
+          </G>
+        </G>
+        <G id="shape-b37a7557-9722-8090-8002-3f19e7f2dfe0">
+          <G id="shape-b37a7557-9722-8090-8002-3f1954ed8ca2" fill="#000">
+            <G id="shape-b37a7557-9722-8090-8002-3f1954f1a2b0" fill="#000">
+              <G id="shape-b37a7557-9722-8090-8002-3f1954f2ba0a" fill="#000">
+                <G id="shape-b37a7557-9722-8090-8002-3f1954f388a5" fill="#000">
+                  <G id="shape-b37a7557-9722-8090-8002-3f1954f742a4">
+                    <G
+                      className="fills"
+                      id="fills-b37a7557-9722-8090-8002-3f1954f742a4">
+                      <Path
+                        fillRule="evenodd"
+                        d="M205 51.353l-.028 56.871c0 16.398-13.298 29.776-29.661 29.776h-56.622C102.326 138 89 124.622 89 108.224V51.353c0-16.426 13.326-29.776 29.689-29.776h20.355L126.673 9.159c-2.08-2.089-2.08-5.504 0-7.593 2.109-2.088 5.483-2.088 7.563 0l12.511 12.56 12.511-12.56a5.315 5.315 0 017.535 0c2.08 2.089 2.08 5.476 0 7.593l-12.371 12.418h20.889c16.363 0 29.661 13.35 29.689 29.776zm-10.683 0c0-10.527-8.491-19.051-18.978-19.051h-56.622c-10.487 0-18.977 8.524-18.977 19.051l-.028 56.871c0 10.499 8.518 19.051 19.005 19.051h56.65c10.459 0 18.978-8.552 18.978-19.051z"
+                        fill="#ffc300"
+                        fillOpacity={1}
+                      />
+                    </G>
+                  </G>
+                </G>
+              </G>
+            </G>
+          </G>
+          <G id="shape-b37a7557-9722-8090-8002-3f19526910ac" fill="#000">
+            <G id="shape-b37a7557-9722-8090-8002-3f195272fd45">
+              <Path
+                d="M133.544 55.831a5.416 5.416 0 00-5.484-.102 5.43 5.43 0 00-2.771 4.733v39.759a5.43 5.43 0 002.771 4.733c1.707.96 3.81.915 5.484-.102l32.566-19.879a5.425 5.425 0 000-9.263l-32.566-19.879z"
+                id="fills-b37a7557-9722-8090-8002-3f195272fd45"
+                fill="#ffc300"
+                fillOpacity={1}
+              />
+            </G>
+          </G>
+        </G>
+      </G>
+    </Svg>
+  );
+};
---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export * from './Onboarding1';
+export * from './Onboarding2';
+export * from './Onboarding3';
+export * from './Onboarding4';
